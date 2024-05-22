26553529270337eb00e213db195da2ed30abf175
prathmesh_patange <prathmeshpatange123@gmail.com>
2024-05-22 23:43:35 +0530

    script.js

--- Ecommerce/public/js/script.js
+++ Ecommerce/public/js/script.js
@@ -1,3 +1,5 @@
+const response  = require("express");
+
 document.addEventListener('DOMContentLoaded', () => {
     // Register form submission
     document.getElementById('registerForm').addEventListener('submit', async (event) => {
@@ -9,14 +11,41 @@ document.addEventListener('DOMContentLoaded', () => {
         const password = document.getElementById('password').value;
         console.log(name,email,dateOfBirth,password);
         try {
-            const response = await fetch('/auth/register', {
+            const response = await fetch('/auth/register', { //fetch data to register
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ name, email, dateOfBirth, password })
             });
         }
         catch{}
-    });
+    }); 
+
+    //Login Form
+    document.getElementById('loginForm').addEventListener('submit',async(event)=>{
+        event.preventDefault();
+
+        const email=document.getElementById('email').value;
+        const password=document.getElementById('password').value;
+        console.log(email,password);
+
+        try{
+            const response=await fetch('/auth/login',{
+                method:'POST',
+                headers:{'Content-type':'application/json'},
+                body:JSON.stringify({email,password})
+            })
+
+            const result=await response.json();
+            if(response.ok){
+                windows.location.href='/homepage.html'
+            }else{
+                alert(result.message || 'Login failed');
+            }
+        }catch(error){
+            console.error('Error during login:',error);
+            alert('Login failed');
+        }
+    })
 
 });
 
